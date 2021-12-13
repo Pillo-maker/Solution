8006b0b9f1b1f91bdd3840b44060297d1386fbbb
Filippo Gaffuri Riva <75850112+Pillo-maker@users.noreply.github.com>
2021-12-13 13:57:13 +0100

    Update app.py
    Added an explanation comment after importing

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from solution import Solution
 
-
+#final app. 
+#Is is only required for the user to input the three path required by the assignement to generate the json and the other files. 
 imagedir = 'insert input dir for images'
 xmldir ='inser input dir for xml files'
 outputdir = 'insert output dir'
